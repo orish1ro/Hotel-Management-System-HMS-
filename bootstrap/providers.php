03deb515ea50964ff2cb8515f8c56ee8bd34b18a
orish1ro <russeljohnlasapraagadio@gmail.com>
2026-05-13 18:53:26 +0800

    added

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,53 +1,7 @@
 <?php
 
+use App\Providers\AppServiceProvider;
 
-
-namespace App\Providers;
-
-
-
-use Illuminate\Support\ServiceProvider;
-
-use Illuminate\Support\Facades\URL;
-
-
-
-class AppServiceProvider extends ServiceProvider
-
-{
-
-    /**
-
-     * Register any application services.
-
-     */
-
-    public function register(): void
-
-    {
-
-        //
-
-    }
-
-
-
-    /**
-
-     * Bootstrap any application services.
-
-     */
-
-    public function boot(): void
-
-    {
-
-        if (env('APP_ENV') === 'production') {
-
-            URL::forceScheme('https');
-
-        }
-
-    }
-
-}
+return [
+    AppServiceProvider::class,
+];
